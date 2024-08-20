69ad1fec778ff93cc27a737fe3ddca01667f47d6
henderson2k <henderson2k@users.noreply.github.com>
2024-08-20 16:05:14 +0100

    Update eKinnex/CopyCarts2.js

--- eKinnex/CopyCarts2.js
+++ eKinnex/CopyCarts2.js
@@ -1,47 +1,36 @@
-const urlToCheck = 'https://stylelink.ergotron.com/app/carts?org=354';
-var currentUrl = window.location.href;
-if (currentUrl === urlToCheck) {
-    (function() {
-        var matchesSet = new Set();
-        var elements = document.querySelectorAll('body *');
-        elements.forEach(function(element) {
-            var text = element.innerText.trim();
-            var regex = /[DM]\d{5}/g;
-            var match;
-            while ((match = regex.exec(text)) !== null) {
-                matchesSet.add(match[0]);
-            }
-        });
+(function() {
+    const matchesSet = new Set();
+    document.querySelectorAll('body *').forEach(element => {
+        const text = element.innerText.trim();
+        const regex = /[DM]\d{5}/g;
+        let match;
+        while ((match = regex.exec(text)) !== null) {
+            matchesSet.add(match[0]);
+        }
+    });
 
-        var allMatches = Array.from(matchesSet).join('\n');
-        navigator.clipboard.writeText(allMatches).then(function() {
+    const allMatches = Array.from(matchesSet).join('\n');
+    navigator.clipboard.writeText(allMatches)
+        .then(() => {
             console.log(allMatches);
             showDialog(allMatches);
-        }).catch(function(error) {
+        })
+        .catch(error => {
             console.error('Error copying text: ', error);
-            alert('Error copying text. See console for details.');
         });
-    })();
-} else {
-    showDialog("Not on Carts Page");
-}
+})();
 
 function showDialog(messageText) {
-    var d = document.createElement('dialog');
+    const d = document.createElement('dialog');
     d.id = 'dialogDefault';
     d.innerHTML = `
         <form method="dialog">
             <p>${messageText}</p>
-         <!--  <button type="submit" value="ok">OK</button> -->
         </form>
-    `
+    `;
     document.body.appendChild(d);
     d.showModal();
 
-    // Automatically close dialog after 3 seconds
-    setTimeout(function() {
-        d.close('timeout');
-    }, 1000);
+    // Automatically close dialog after 1 second
+    setTimeout(() => d.close('timeout'), 1000);
 }
-
-
